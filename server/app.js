1b7513ebaf7d2aa9e3b88f084f1b7980993e9dac
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2024-03-02 12:32:49 +0530

    Update app.js
    Created server running on 3000 port and created route to the different database models used to store data

--- server/app.js
+++ server/app.js
@@ -1,1 +1,28 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const authRoute = require("./router/auth-router");
+const PitchRoute = require("./router/pitch-router");
+dotenv.config({ path: './config.env' });
+const errorMiddleWare = require("./middlewares/error-middleware");
+
+const corsOptions = {
+    origin: "http://localhost:5173",
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+require('./db/conn');
+app.use("/api/auth", authRoute);
+app.use("/api/pitches", PitchRoute);
+app.use(errorMiddleWare);
+
+app.listen(3000, function (req, res) {
+    console.log("server running on port 3000");
+});
 
