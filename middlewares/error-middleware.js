da7ff0dc69744d6624150b2386a15ea8724c8648
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2024-03-02 14:51:21 +0530

    Delete middlewares directory
    deleted all middlewares files

--- middlewares/error-middleware.js
+++
@@ -1,9 +0,0 @@
-const errorMiddleWare = (err,req,res,next) =>{
-    const status = err.status || 500;
-    const message = err.message || "Backend Error";
-    const extraDetails = err.extraDetails || "Error from Backend";
-
-    return res.status(status).json({message : extraDetails});
-}
-
-module.exports = errorMiddleWare;
